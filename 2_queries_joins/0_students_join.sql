9807ce8d8c96dae9149c272b45b9e785b63f3d12
Dihni Hassan <dihnihassan@gmail.com>
2022-12-12 17:59:33 -0500

    Create Join tables

---
+++ 2_queries_joins/0_students_join.sql
@@ -0,0 +1,3 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM students JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE name = 'Ibrahim Schimmel';
