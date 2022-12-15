5e46bb8888213abf7fbb8a466d5ab38efb20d8b6
Dihni Hassan <dihnihassan@gmail.com>
2022-12-14 22:33:26 -0500

    SQL injection with possible error

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,39 @@ const pool = new Pool({
 
 
 
-pool.query(`
-SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
+// pool.query(`
+// SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
+// FROM assistance_requests
+// JOIN teachers ON assistance_requests.teacher_id = teachers.id
+// JOIN students ON assistance_requests.student_id = students.id
+// JOIN cohorts ON students.cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teachers.name;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+
+
+
+const queryString = `SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+`
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })  
+// });  
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [`%${cohortName}%`];
+pool.query(queryString, values);
